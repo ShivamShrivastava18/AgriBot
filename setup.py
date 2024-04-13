798075292ccac65ccbe669b83e9635c98577506a
Vastav <shrivastav.shivam1803@gmail.com>
2024-04-13 13:32:14 +0530

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="Agribot",
+    version="0.0.0",
+    author="Shivam Shrivastava",
+    author_email="shrivastav.shivam1803@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
